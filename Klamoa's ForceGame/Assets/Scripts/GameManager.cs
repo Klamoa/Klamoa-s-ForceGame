2da1dee0852a26859d20fdf8ba74c41ddb0b6583
Klamoa <benjamin-maier@gmx.at>
2018-04-02 00:54:52 +0200

    1

--- Klamoa's ForceGame/Assets/Scripts/GameManager.cs
+++ Klamoa's ForceGame/Assets/Scripts/GameManager.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour {
 
 	public static int score = 0;
-	public Text scoreText;
+	public TextMeshProUGUI scoreText;
 
 	public int inspectorScore;
 

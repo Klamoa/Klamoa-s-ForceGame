0fd83ec54fcef79e2ca6a981de2cf4ef13aa5a2b
Klamoa <benjamin-maier@gmx.at>
2018-08-05 18:48:19 +0200

    1

---
+++ Klamoa's ForceGame/Assets/Scripts/BridAnimationController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridAnimationController : MonoBehaviour {
+
+	public int animationNumber;
+	public float waitTime;
+
+	private Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator>();
+		anim.SetInteger("animationNumber", 0);
+		StartCoroutine(DelayAnimation(waitTime));
+	}
+
+	IEnumerator DelayAnimation (float _waitTime) {
+
+		yield return new WaitForSeconds(_waitTime);
+		anim.SetInteger("animationNumber", animationNumber);
+	}
+
+}
